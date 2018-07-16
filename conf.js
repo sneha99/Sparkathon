b8f85754fd516063015a18560239a8c115eaa0b6
Nakita Oza <nakita.oza@nice.com>
2018-07-16 20:29:14 +0530

    Update conf.js

--- conf.js
+++ conf.js
@@ -9,15 +9,7 @@ exports.config = {
             saveSuccessVideos: true,
 			createSubtitles: true,
 			singleVideo : false,
-            ffmpegCmd: "C:\\ffmpeg\\bin\\ffmpeg.exe",
-            singleVideoPath : 'fullName',
-            ffmpegArgs: [
-			  '-y',
-			  '-f', 'gdigrab',
-			  '-i', 'desktop',
-			  '-vcodec', 'libx264',
-			  '-filter:v', 'setpts=2.0*PTS'
-			  ] 
+            singleVideoPath : 'fullName'
         }));
 }
 };
